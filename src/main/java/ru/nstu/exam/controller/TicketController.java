7defa3659d2ebe4e9c5bd8cfd673410c82b0302f
Vladislav Samorokov <batman_154@mail.ru>
2022-03-20 19:10:22 +0700

    fix

--- src/main/java/ru/nstu/exam/controller/TicketController.java
+++ src/main/java/ru/nstu/exam/controller/TicketController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.nstu.exam.bean.TicketBean;
-import ru.nstu.exam.security.IsAdmin;
+import ru.nstu.exam.security.IsTeacher;
 import ru.nstu.exam.service.TicketService;
 
 import java.util.List;
@@ -19,7 +19,7 @@ public class TicketController {
 
     private final TicketService ticketService;
 
-    @IsAdmin
+    @IsTeacher
     @PostMapping("/rating")
     public void updateTicketsRating(@RequestBody List<TicketBean> ticketBeans) {
         ticketService.update(ticketBeans);
