5da62c3ab55c23d61cc4d3e3a84b459aa8e54084
Vladislav Samorokov <batman_154@mail.ru>
2022-04-22 17:06:03 +0700

    fix....

--- src/main/java/ru/nstu/exam/service/mapper/FullDisciplineMapper.java
+++ src/main/java/ru/nstu/exam/service/mapper/FullDisciplineMapper.java
@@ -33,6 +33,6 @@ public class FullDisciplineMapper implements Mapper<FullDisciplineBean, Discipli
             bean.setThemes(themeBeans);
         }
 
-        return null;
+        return bean;
     }
 }
